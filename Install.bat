e4177b2168e61aa0017f14a6daca09d3e42a404a
Brahman <121321688+DJ-PAIN@users.noreply.github.com>
2025-01-05 02:11:45 -0500

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,6 +1,7 @@
 @echo off
 echo Welcome to KittyCDN!
 echo Make sure you have Python and pip installed.
+echo GIT IS NEED FOR UPDATES MAKE SURE YOU HAVE IT INSTALLED!!.
 echo If everything looks good, let's proceed...
 
 :: Install Python dependencies
